63a4c1f88bc42581569f7174813d595597c9b43b
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-09-21 17:51:55 +0200

    Initial Commit

---
+++ Algorithms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Algorithms
+//
+//  Created by Marek Skrzelowski on 21/09/2019.
+//  Copyright © 2019 vandermesis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
